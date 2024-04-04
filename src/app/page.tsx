a862a7bb9b8c330479fd50e65c3d93802dfe23ad
tofuminto <tototofumifumin@gmail.com>
2024-04-05 01:04:58 +0900

    feature/add-activation-of-tab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,69 @@
-import Image from "next/image";
+"use client"
+
+import Image from 'next/image';
+import React, { useState } from 'react';
+
+type TabIdentifier = 'lv1' | 'lv25' | 'lv50' | 'lv75';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [activeTab, setActiveTab] = useState<TabIdentifier>('lv1');
+  const levels: TabIdentifier[] = ['lv1', 'lv25', 'lv50', 'lv75'];
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+  const switchTab = (newTab: TabIdentifier) => {
+    setActiveTab(newTab);
+  };
+
+  return (
+    <main>
+      <div className="header">
+        <p className="headerTitle">OctoHub<span className="headerSubTitle">オクトパストラベラー ~大陸の覇者~</span></p>
+      </div>
+      <div className="mainFrame">
+        <p className="mainTitle">ボス一覧</p>
+        <div className="border-b border-slate-200">
+          <ul className="flex flex-wrap -mb-px text-sm font-medium text-center" role="tablist">
+            {levels.map((level) => (
+              <li key={level} className="me-2" role="presentation">
+                <button
+                  className={`inline-block p-4 border-b-2 rounded-t-lg ${activeTab === level ? 'text-slate-700 border-slate-700' : 'hover:text-slate-400 hover:border-slate-400'}`}
+                  onClick={() => switchTab(level)}
+                  role="tab"
+                  aria-selected={activeTab === level}
+                >
+                  {`Lv.${level.substring(2)}~`}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div id="level-tab-content" className="pt-4">
+          {levels.map((level) => (
+            <div
+              key={level}
+              className={`p-4 rounded-lg ${activeTab === level ? 'bg-slate-100' : 'hidden bg-slate-50'}`}
+              role="tabpanel"
+              aria-labelledby={`${level}-tab`}
+            >
+              <p className="text-sm text-slate-500">
+                This is some placeholder content for the <strong className="font-medium text-slate-900">{`${level.substring(2)} tab's associated content`}</strong>. Clicking another tab will toggle the visibility of this one for the next. The tab JavaScript swaps classNamees to control the content visibility and styling.
+              </p>
+            </div>
+          ))}
+        </div>
+
+        <p className="topContent">Level Tab</p>
+        <p className="topContent">Boss View</p>
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          src="/noimage.svg"
+          alt="enemy.boss.icon"
+          className=""
+          width={ 100 }
+          height={ 100 }
           priority
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <p className="topContent">オクトラの写記をイメージして、作成。ボスをクリックしたら別ページで左に敵を表示し右にパーティー編成をいいね数ランキングにして表示</p>
+        <p className="topContent">ボスの情報は名前とExなどのランクと弱点を表示したい。行動などの詳細は今後実装予定にしようかな</p>
+        <p className="topContent">※ボスをクリックしたら、パーティー編成のいいね数ごとのランキングを昇順で表示。また自身も作成できるようにCreateを表示</p>
       </div>
     </main>
   );
